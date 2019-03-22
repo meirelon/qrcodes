14aa4d7fdbbfd7353641159aab24f716a57e70a0
meirelon <nestelm@gmail.com>
2019-03-22 18:08:46 -0400

    updated qrcode script to fix bug

--- qrcode.py
+++ qrcode.py
@@ -1,9 +1,10 @@
+import os
 import argparse
 import pyqrcode
 
 def qrcode(link, scale=8):
-    url=pyqrcode.creat(link)
-    url.png('qrcodes/qrcode.png', scale=scale)
+    url=pyqrcode.create(link)
+    url.png('qrcode.png', scale=scale)
 
 def main(argv=None):
     parser = argparse.ArgumentParser()
@@ -12,8 +13,13 @@ def main(argv=None):
                         dest='link',
                         default=None,
                         help='insert a link')
+    parser.add_argument('--scale',
+                        dest='scale',
+                        default=int(8),
+                        help='scale of QR code')
+
     args, _ = parser.parse_known_args(argv)
-    qrcode(args.link)
+    qrcode(link=args.link, scale=args.scale)
 
 if __name__ == '__main__':
     main()
