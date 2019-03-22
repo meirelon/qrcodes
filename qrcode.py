0ee417a5000e2588567a2146ae400f304404b464
meirelon <nestelm@gmail.com>
2019-03-22 17:56:45 -0400

    adding qrcode script

---
+++ qrcode.py
@@ -0,0 +1,19 @@
+import argparse
+import pyqrcode
+
+def qrcode(link, scale=8):
+    url=pyqrcode.creat(link)
+    url.png('qrcode.png', scale=scale)
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--link',
+                        dest='link',
+                        default=None,
+                        help='insert a link')
+    args, _ = parser.parse_known_args(argv)
+    qrcode(args.link)
+
+if __name__ == '__main__':
+    main()
